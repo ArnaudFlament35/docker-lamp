d8c627291f318f52c613ed563c3163d40fdaf041
Arnaud Flament <arnaudflam@gmail.com>
2025-03-31 11:16:58 +0200

    first commit

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php 
+phpinfo();
+die;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Voici l'app 1</h1>
+</body>
+</html>
