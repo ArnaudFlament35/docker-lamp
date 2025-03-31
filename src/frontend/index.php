368ef87ed3df6ef6282292380a837dde7766ba25
Arnaud Flament <arnaudflam@gmail.com>
2025-03-31 14:21:02 +0200

    add fullstack site with frontend and backend

--- src/frontend/index.php
+++ src/frontend/index.php
@@ -1,13 +1,14 @@
 <?php 
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Frontend</title>
 </head>
 <body>
-    
+    <h1>Frontend</h1>
 </body>
 </html>
